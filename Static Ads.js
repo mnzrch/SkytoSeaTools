173ee6f476710282fc6593a8dfc573bf6d68b23f
Mnzr chauhan <95564253+mnzrch@users.noreply.github.com>
2022-03-04 10:49:38 +0500

    Add files via upload

--- Static Ads.js
+++ Static Ads.js
@@ -17,22 +17,19 @@ if (id('custom-footer-ad') != null) {
 if (id('custom-footer-ctrl') != null) {
     id('custom-footer-ctrl').addEventListener('click', () => {
         if (id('custom-footer-ctrl').innerHTML == '↓') {
-                id('custom-footer-ad').style.bottom=-100+'px';
-//                 id('custom-footer-ad').style.bottom= -id('custom-footer-ad').clientHeight;
             setTimeout(() => {
                 id('custom-footer-ctrl').innerHTML = '↑';
             }, 500);
-//             if (id('custom-footer-ad') != null) {
-//                 id('custom-footer-ad').style.bottom=-id('custom-footer-ad').clientHeight;
-//             }
+            if (id('custom-footer-ad') != null) {
+                id('custom-footer-ad').style.bottom=-id('custom-footer-ad').clientHeight;
+            }
         } else if (id('custom-footer-ctrl').innerHTML == '↑') {
-                id('custom-footer-ad').style.bottom=0;
             setTimeout(() => {
                 id('custom-footer-ctrl').innerHTML = '↓';
             }, 500);
-//             if (id('custom-footer-ad') != null) {
-//                 id('custom-footer-ad').style.bottom=0;
-//             }
+            if (id('custom-footer-ad') != null) {
+                id('custom-footer-ad').style.bottom=0;
+            }
         } else {
             return false;
         }
