dbbcae2e4816042e03bc70bb27b425b608331860
Mnzr chauhan <95564253+mnzrch@users.noreply.github.com>
2022-03-07 07:23:34 +0500

    Add files via upload

--- classes.js
+++ classes.js
@@ -38,38 +38,45 @@ if (cls('btnv1') != null) {
     });
 }
 //            Overlay
-setTimeout(() => {
+let fixoverlay = setInterval(() => {
     if (id('fix-overlay') != null) {
+        clearInterval(fixoverlay);
         id('fix-overlay').setAttribute('class', 'fix top-0 right-0 left-0 bottom-0 padding-10 bg-black index-997');
         id('fix-overlay').innerHTML = id('fix-overlay').innerHTML + '<span id="fix-overlay-ctrl" class="margin-5 fix right-0 top-0 btnv1">⨯</span>';
-        let fixoverlayctrlt = id('fix-overlay').getAttribute('wait');
-        fixoverlayctrl = setInterval(() => {
-            fixoverlayctrlt -= 1;
-            if (fixoverlayctrlt <= 0) {
-                clearInterval(fixoverlayctrl);
-                id('fix-overlay-ctrl').innerHTML = '⨯';
-                id('fix-overlay-ctrl').style.cursor = 'pointer';
-            } else {
-                id('fix-overlay-ctrl').innerHTML = fixoverlayctrlt + 's';
-            }
-        }, 1000)
-        setTimeout(() => {
-            if (id('fix-overlay-ctrl') != null) {
-                id('fix-overlay-ctrl').addEventListener('click', () => {
-                    if (id('fix-overlay-ctrl').innerHTML == '⨯') {
-                        id('fix-overlay').remove();
-                    }
-                })
-            }
-        }, 1000);
+        if (window.location.href.match('mywebapp') != null) {
+            id('fix-overlay-ctrl').remove();
+            id('fix-overlay').style.background = 'black';
+        } else {
+            let fixoverlayctrlt = id('fix-overlay').getAttribute('wait');
+            fixoverlayctrl = setInterval(() => {
+                fixoverlayctrlt -= 1;
+                if (fixoverlayctrlt <= 0) {
+                    clearInterval(fixoverlayctrl);
+                    id('fix-overlay-ctrl').innerHTML = '⨯';
+                    id('fix-overlay-ctrl').style.cursor = 'pointer';
+                } else {
+                    id('fix-overlay-ctrl').innerHTML = fixoverlayctrlt + 's';
+                }
+            }, 1000)
+            setTimeout(() => {
+                if (id('fix-overlay-ctrl') != null) {
+                    id('fix-overlay-ctrl').addEventListener('click', () => {
+                        if (id('fix-overlay-ctrl').innerHTML == '⨯') {
+                            id('fix-overlay').remove();
+                        }
+                    })
+                }
+            }, 1000);
+        }
     }
-}, 1000)
+}, 10)
 //            footer ad
-setTimeout(() => {
+let fixfooterad = setInterval(() => {
     if (id('fix-footer-ad') != null) {
+        clearInterval(fixfooterad);
         id('fix-footer-ad').innerHTML = '<div id="custom-footer-ad" class="w-inherit fix h-25pc maxh-250 minh-100 right-0 bottom-0 left-0 index-996"><span id="custom-footer-ctrl-shadow"></span><span id="custom-footer-ctrl">▽</span><div id="custom-footer-adhere" class="w-wfa h-wfa margin-5 of-hide bg-aliceblue">Adhere</div></div>';
         if (id('custom-footer-adhere') != null && id('fix-footer-ad-content') != null) {
-            id('custom-footer-adhere').innerHTML=id('fix-footer-ad-content').innerHTML.replace('<!--','').replace('-->','');
+            id('custom-footer-adhere').innerHTML = id('fix-footer-ad-content').innerHTML.replace('<!--', '').replace('-->', '');
         }
         if (id('custom-footer-ad') != null) {
             window.addEventListener('resize', () => {
@@ -114,4 +121,4 @@ setTimeout(() => {
             }, 1000);
         }
     }
-}, 1000)
+}, 10)
