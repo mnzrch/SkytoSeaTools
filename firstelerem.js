dbf3bdab6207d042e39d308771990df7c3cad686
Mnzr chauhan <95564253+mnzrch@users.noreply.github.com>
2022-03-10 09:30:45 +0500

    Delete firstelerem.js

--- firstelerem.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("*").click(function (e) {
-    e.preventDefault();
-    document.querySelector(this)[0].remove();
-    document.querySelector('#removemeonclick')
-    return false;
-});
