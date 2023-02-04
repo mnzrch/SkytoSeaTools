92c538dc34beea1db99f2560dfe6fb7c643523be
Mnzr chauhan <95564253+mnzrch@users.noreply.github.com>
2023-02-04 20:18:32 +0500

    Add files via upload

--- SkytoSeeToolsv1.js
+++ SkytoSeeToolsv1.js
@@ -54,13 +54,12 @@ const scripts = () => {
         window.onresize = function () {
             lll_listerH();
         }
-        html('<div class="lll-item-lister-' + lll_lister_index + ' lll-item-lister of-hidden h-inherit w-wfa lightboxW"> <div class="header dis-flex w-wfa bg-gray padding-2 jc-spacebetween"> <span id="title" class="margin-2 lightele w-wfa drager"><div class="relative">Auto Generated List</div></span> <span id="close" title="Click to close it!" class="btnv3 htmlcross"></span> </div> <div class="items dis-flex ai-center fd-column of-auto"></div> </div>', ele);
         if (getAttr('name', ele)) {
-            let titlediv = $(".lll-item-lister-" + lll_lister_index + ">.header>#title>div");
-            html(getAttr('name', ele), titlediv);
-            console.log(titlediv);
-            setAttr('title', titlediv.innerHTML, titlediv);
+            let titlediv = getAttr('name', ele);
+        } else {
+            let titlediv = 'Auto Generated List';
         }
+        html('<div class="lll-item-lister-' + lll_lister_index + ' lll-item-lister of-hidden h-inherit w-wfa lightboxW"> <div class="header dis-flex w-wfa bg-gray padding-2 jc-spacebetween"> <span id="title" class="margin-2 lightele w-wfa drager"><div class="relative" title="' + titlediv + '">' + titlediv + '</div></span> <span id="close" title="Click to close it!" class="btnv3 htmlcross"></span> </div> <div class="items dis-flex ai-center fd-column of-auto"></div> </div>', ele);
         document.querySelector(".lll-item-lister-" + lll_lister_index + ">.header>#close").onclick = function () {
             setCSS('display', 'none', ele);
         }
